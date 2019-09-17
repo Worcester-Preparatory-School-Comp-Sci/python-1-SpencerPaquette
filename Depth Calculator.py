1356fe7b0dbda9c01a02132666b85cb2ea3e27a8
Spencer Paquette <spaquette@wpsmd.org>
2019-09-17 14:01:20 -0400

    Python 1
    Python 1

---
+++ Depth Calculator.py
@@ -0,0 +1,6 @@
+def depthCalc():
+    lakeVol = float(input("What is the volume of the great lakes"))
+    usArea = float(input("What is the area of the contiguous United States"))
+    depth = lakeVol / usArea
+    print("The new depth is:", depth)
+depthCalc()
---
+++ Gas Converter.py
@@ -0,0 +1,11 @@
+def gasConverter():
+    gasGallons = float(input("How many gallons of gas are you using?"))
+    gasLiters = gasGallons * 3.78541178
+    oilBarrels = gasGallons / 19.5
+    carbonProduced = gasGallons * 20
+    price = gasGallons * 3.35
+    print("Liters:", gasLiters)
+    print("Number of oil barrels:", oilBarrels)
+    print("Pounds of CO2 produced:", carbonProduced)
+    print("Price in USD:", price)
+gasConverter()
